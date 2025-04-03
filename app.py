756af0952439c64766b27321fb9261f912e97fd4
Marek <marek_ski@hotmail.co.uk>
2025-04-03 18:29:25 +0100

    merging

--- app.py
+++ app.py
@@ -1,9 +1,60 @@
-from flask import Flask, render_template
+import os
+
+from helpers import login_required
+
+from flask import Flask, render_template, session, request, redirect, flash
+from flask_session import Session
+
+from sqlalchemy import create_engine
+import sqlite3 
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
 app = Flask(__name__)
 
-message = "hello?"
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("/")
-def main():
+@login_required
+def index():
 
-    return render_template("index.html", message=message)
+    if session["user_id"]:
+        return render_template("layout.html")
+    else:
+        redirect("/login")
+
+
+@app.route("/login" , methods=["GET", "POST"])
+def login():
+
+    #clear any session info
+    session.clear()
+
+    if request.method == "POST":
+        if not request.form.get("username"):
+            flash("Please enter a username")
+            return render_template("login.html")
+        
+        if not request.form.get("password"):
+            flash("Please enter a password")
+            return render_template("login.html")
+        
+        # Query database for username
+
+            #if username exists, attempt to match against password
+        
+        # if login details ok - set session id
+        session["user_id"] = 1 #db query userid
+        return redirect("/")
+    else:
+        return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+
+    this = "is for testing"
+    session.clear()
+    return redirect("/")
---
+++ readme.md
@@ -0,0 +1,31 @@
+
+# Stock Game
+#### Video Demo:  <URL HERE>
+#### Description:
+
+if i kil terminal
+.venv\scripts\activate\ps1
+
+TODO
+# [] Front End
+     [] Landing Page
+
+     [] Buy Page 
+
+     [] Sell Page
+
+     [] Account Page
+
+     [] Quotes/Live Info page
+
+     [] Styling
+        Tailwind?
+
+# [] Back End
+    [] Database configuration
+        SQL alchemy?
+        or just raw python DB-API? - this returns database data as      objects, dict, list etc
+
+    [] Page Code 
+
+    [] Login Functionality
