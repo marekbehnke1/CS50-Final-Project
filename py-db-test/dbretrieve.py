d70947b91a6d134edb4993da92e54aca1a541405
Marek <marek_ski@hotmail.co.uk>
2025-04-03 16:54:57 +0100

    getting there with dict retrieval

--- py-db-test/dbretrieve.py
+++ py-db-test/dbretrieve.py
@@ -1,9 +1,10 @@
 import sqlite3
 
 con = sqlite3.connect("database.db")
+con.row_factory = sqlite3.Row
 cur = con.cursor()
 
 #return query
-res = cur.execute("SELECT * FROM users WHERE userid = 1").fetchall()
+res = cur.execute("SELECT * FROM users WHERE userid = 1").fetchall()[0]
 
-print(res[0])
+print(res["fname"])
